df2fbc2abf7c86c1b7375f3863a68548e60dc9df
Vijay kumar hakyale <92971346+Vijayhakyale@users.noreply.github.com>
2022-06-24 15:42:08 +0530

    adedd form of case fields

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-const SLACK_SIGNING_SECRET = 'bf687f6c049a0a4d594a2a15c99efc0b';
-const SLACK_BOT_TOKEN = 'xoxb-2343587270560-3658335849427-KP68OhZjq1BbpwjYKpiX6pE0';
+const SLACK_SIGNING_SECRET = '9ac775b58e73483dfedd6cc17ab9af09';
+const SLACK_BOT_TOKEN = 'xoxb-2343587270560-3658335849427-aVJB8vJCEmER0JFnZ4OoEiK5';
 const { App } = require('@slack/bolt');
-const request = require('request');
+//const request = require('request');
 const app = new App({
     token:SLACK_BOT_TOKEN,
     signingSecret:SLACK_SIGNING_SECRET,
-   port:process.env.PORT || 3000
+  //port:process.env.PORT || 3000
 });
 (async () => {
     //START YOUR APP
@@ -14,31 +14,31 @@ const app = new App({
     console.log('bolt app is running')
 })();
 
-const options = {
-    url: 'https://raagvitech127-dev-ed.lightning.force.com/services/data/v48.0/sobjects/Case',
-    json: true,
-    body: {
-        Subject: 'Hello world',
-        Description: 'Lorem ipsum dolor sit amet...',
-        Origin: 'Web',
-        AccountId: '0015g00000HvK07AAF'
-    }
-};
+// const options = {
+//     url: 'https://raagvitech127-dev-ed.lightning.force.com/services/data/v48.0/sobjects/Case',
+//     json: true,
+//     body: {
+//         Subject: 'Hello world',
+//         Description: 'Lorem ipsum dolor sit amet...',
+//         Origin: 'Web',
+//         AccountId: '0015g00000HvK07AAF'
+//     }
+// };
 
-request.post(options, (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(`Status: ${res.statusCode}`);
-    console.log(body);
-});
+// request.post(options, (err, res, body) => {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     console.log(`Status: ${res.statusCode}`);
+//     console.log(body);
+// });
 
 
 app.message('Good Morning', async ({ message, say }) => {
     // Start your app
 
     await say(`Hey <@${message.user}>!  Good Morning`)
-     await app.start(process.env.PORT || 3000);
+     //await app.start(process.env.PORT || 3000);
 
     console.log('<@${message.user}> ', message.user);
 })
@@ -59,23 +59,35 @@ app.action('button_click', async ({ body, ack, say }) => {
                 "fields": [
                     {
                         "type": "mrkdwn",
+                        "text": "*Account:*\nBurlington Textiles Corp of America"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Contact:*\nJack Rogers"
+                    },
+                    {
+                        "type": "mrkdwn",
                         "text": "*Type:*\nComputer (Mobile)"
                     },
                     {
                         "type": "mrkdwn",
-                        "text": "*Case orgin:Phone"
+                        "text": "*Case Origin:*\nPhone"
                     },
                     {
                         "type": "mrkdwn",
-                        "text": "*Status:New"
+                        "text": "*Status:*\nNew"
                     },
                     {
                         "type": "mrkdwn",
-                        "text": "*Priority:High"
+                        "text": "*Priority:*\nHigh"
                     },
                     {
                         "type": "mrkdwn",
-                        "text": "*Case Resone:Installation"
+                        "text": "*Case Resone:*\nInstallation"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Product:*\nOne Pluse Pro 5G "
                     }
                 ]
             },
@@ -101,6 +113,158 @@ app.action('button_click', async ({ body, ack, say }) => {
                         },
                         "style": "danger",
                         "value": "click_me_123"
+                    }
+                ]
+            }
+        ]
+    }
+    );
+  });
+  app.action('button_click', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`,{
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": "You have a New case record."
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Account"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Contact"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Type"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Case Reason"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Case Origin"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Status"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Priority"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "datepicker",
+                    "initial_date": "2020-01-08",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select a date"
+                    }
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Date of expense"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Product"
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input",
+                    "multiline": true
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Description"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Close",
+                            "emoji": true
+                        },
+                        "style": "danger",
+                        "value": "approve"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Submit",
+                            "emoji": true
+                        },
+                        "url": "https://raagvitech127-dev-ed.lightning.force.com/lightning/o/Case/list?filterName=Recent",
+                        "style": "primary",
+                        "value": "click_me_123",
+                        "action_id": "actionId-0"
                     }
                 ]
             }
